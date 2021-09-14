368f11631e2137f09a33654dafba0b2f6c788cce
Daricyos <daricyos@gmail.com>
2021-09-14 20:49:31 +0300

    ---

--- students/models.py
+++ students/models.py
@@ -5,7 +5,7 @@ class Student(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     age = models.IntegerField(default=18)
-    phone = models.BigIntegerField(null=True)
+    phone = models.BigIntegerField(max_length=15, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}, {self.age}, {self.phone}"
